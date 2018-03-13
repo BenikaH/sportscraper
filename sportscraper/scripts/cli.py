8410f7104bcdc71f6b9a43348ec9e8e797274741
nubbthedestroyer <michael@desktopninjas.com>
2018-03-12 23:40:40 -0500

    working pitching table import

--- sportscraper/scripts/cli.py
+++ sportscraper/scripts/cli.py
@@ -1,9 +1,10 @@
 import click
 
 # local packages
-from .brooks import *
-from .statcast import *
+from brooks import *
+from statcast import *
 from db_connector import *
+from pitching import *
 
 
 @click.group()
@@ -15,18 +16,6 @@ def cli():
 @click.option('--hostname', '-h', default='nothing')
 def test(hostname):
     click.echo('testing input hostname = ' + hostname)
-
-
-# @click.command('--path_to_csv', '-c')
-# def brooks_echo(path_to_csv):
-#     cmd_brooks_echo(path_to_csv)
-
-
-@click.command()
-@click.option('--start_date', '-s')
-@click.option('--end_date', '-e')
-def statcast_echo(start_date, end_date):
-    cmd_statcast_echo(start_date, end_date)
 
 
 @click.command()
@@ -42,6 +31,18 @@ def statcast_upload(start_date, end_date, db_username, db_password, db_hostname,
 
 
 @click.command()
+@click.option('--start_year', '-s')
+@click.option('--end_year', '-e')
+@click.option('--db_username', '-u')
+@click.option('--db_password', '-p')
+@click.option('--db_hostname', '-h')
+@click.option('--db_name', '-d')
+@click.option('--db_tablename', '-t')
+def pitching_upload(start_year, end_year, db_username, db_password, db_hostname, db_name, db_tablename):
+    cmd_pitching_upload(start_year, end_year, db_username, db_password, db_hostname, db_name, db_tablename)
+
+
+@click.command()
 @click.option('--path_to_csv', '-c')
 @click.option('--db_username', '-u')
 @click.option('--db_password', '-p')
@@ -54,8 +55,7 @@ def brooks_upload(path_to_csv, db_username, db_password, db_hostname, db_name, d
 
 # add block of commands
 cli.add_command(test)
-cli.add_command(statcast_echo)
 cli.add_command(statcast_upload)
-# cli.add_command(brooks_echo)
 cli.add_command(brooks_upload)
+cli.add_command(pitching_upload)
 
---
+++ sportscraper/scripts/pitching.py
@@ -0,0 +1,23 @@
+from pybaseball import statcast, pitching_stats
+from db_connector import *
+import pandas as pd
+
+import click
+
+
+def cmd_pitching_upload(start_year, end_year, db_username, db_password, db_hostname, db_name, db_tablename):
+
+    click.echo('[[[ PULLING PITCHING DATAFRAME ]]]')
+
+    engine = initdb_pitching(db_username, db_password, db_hostname, db_name, db_tablename)
+
+    click.echo(str('Pulling data...'))
+    try:
+        data = pitching_stats(start_year, end_year)
+        data.columns = data.columns.str.replace('%', '')
+        upload_block(data, engine, db_tablename)
+    except Exception as exc:
+        click.echo("ERROR pulling down data - Error was = " + str(exc))
+    else:
+        click.echo(str('SUCCESS, pulled ' + str(data.shape[0]) + ' records'))
+
